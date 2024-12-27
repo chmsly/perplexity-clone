59ffc5dad3beb886239b2db2a26d39dd8b6b524d
chmsly <chris.c.hemsley@gmail.com>
2024-12-27 12:51:58 -0700

    webhooks/route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,57 +1,92 @@
-import { Webhook } from "svix"
+import {
+  manageSubscriptionStatusChange,
+  updateStripeCustomer
+} from "@/actions/stripe-actions"
+import { stripe } from "@/lib/stripe"
 import { headers } from "next/headers"
-import { WebhookEvent } from "@clerk/nextjs/server"
-import { db } from "@/db/db"
-import { profilesTable } from "@/db/schema"
+import Stripe from "stripe"
+
+const relevantEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.updated",
+  "customer.subscription.deleted"
+])
 
 export async function POST(req: Request) {
-  // Get the headers
-  const headerPayload = headers()
-  const svix_id = headerPayload.get("svix-id")
-  const svix_timestamp = headerPayload.get("svix-timestamp")
-  const svix_signature = headerPayload.get("svix-signature")
+  const body = await req.text()
+  const sig = headers().get("Stripe-Signature") as string
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET
+  let event: Stripe.Event
 
-  // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400
-    })
-  }
-
-  // Get the body
-  const payload = await req.json()
-  const body = JSON.stringify(payload)
-
-  // Create a new Svix instance with your webhook secret
-  const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET || "")
-
-  let evt: WebhookEvent
-
-  // Verify the webhook
   try {
-    evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature
-    }) as WebhookEvent
-  } catch (err) {
-    console.error("Error verifying webhook:", err)
-    return new Response("Error occured", {
-      status: 400
-    })
+    if (!sig || !webhookSecret) {
+      throw new Error("Webhook secret or signature missing")
+    }
+
+    event = stripe.webhooks.constructEvent(body, sig, webhookSecret)
+  } catch (err: any) {
+    console.error(`Webhook Error: ${err.message}`)
+    return new Response(`Webhook Error: ${err.message}`, { status: 400 })
   }
 
-  // Handle the webhook
-  const eventType = evt.type
+  if (relevantEvents.has(event.type)) {
+    try {
+      switch (event.type) {
+        case "customer.subscription.updated":
+        case "customer.subscription.deleted":
+          await handleSubscriptionChange(event)
+          break
 
-  if (eventType === "user.created") {
-    const { id } = evt.data
+        case "checkout.session.completed":
+          await handleCheckoutSession(event)
+          break
 
-    // Create a new profile
-    await db.insert(profilesTable).values({
-      userId: id
-    })
+        default:
+          throw new Error("Unhandled relevant event!")
+      }
+    } catch (error) {
+      console.error("Webhook handler failed:", error)
+      return new Response(
+        "Webhook handler failed. View your nextjs function logs.",
+        {
+          status: 400
+        }
+      )
+    }
   }
 
-  return new Response("", { status: 200 })
+  return new Response(JSON.stringify({ received: true }))
+}
+
+async function handleSubscriptionChange(event: Stripe.Event) {
+  const subscription = event.data.object as Stripe.Subscription
+  const productId = subscription.items.data[0].price.product as string
+  await manageSubscriptionStatusChange(
+    subscription.id,
+    subscription.customer as string,
+    productId
+  )
+}
+
+async function handleCheckoutSession(event: Stripe.Event) {
+  const checkoutSession = event.data.object as Stripe.Checkout.Session
+  if (checkoutSession.mode === "subscription") {
+    const subscriptionId = checkoutSession.subscription as string
+    await updateStripeCustomer(
+      checkoutSession.client_reference_id as string,
+      subscriptionId,
+      checkoutSession.customer as string
+    )
+
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId, {
+      expand: ["default_payment_method"]
+    })
+
+    const productId = subscription.items.data[0].price.product as string
+    await manageSubscriptionStatusChange(
+      subscription.id,
+      subscription.customer as string,
+      productId
+    )
+  }
 }
