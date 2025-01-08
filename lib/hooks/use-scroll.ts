17644819ac275eed1d03d75c43bb17210444af03
chmsly <chris.c.hemsley@gmail.com>
2025-01-07 17:16:47 -0700

    create and implement use-scroll.ts

---
+++ lib/hooks/use-scroll.ts
@@ -0,0 +1,10 @@
+"use client"
+
+import { useEffect } from "react"
+
+export function useScroll(callback: () => void) {
+  useEffect(() => {
+    window.addEventListener("scroll", callback)
+    return () => window.removeEventListener("scroll", callback)
+  }, [callback])
+}
