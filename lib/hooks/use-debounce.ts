151da15d5d801a039cc1aed7a0669704e7d893ff
chmsly <chris.c.hemsley@gmail.com>
2025-01-17 14:55:05 -0700

    create lib/hooks/use-debounce.ts for debouncing user input

--- lib/hooks/use-debounce.ts
+++ lib/hooks/use-debounce.ts
@@ -1,3 +1,5 @@
+"use client"
+
 import { useEffect, useState } from "react"
 
 export function useDebounce<T>(value: T, delay?: number): T {
@@ -12,4 +14,23 @@ export function useDebounce<T>(value: T, delay?: number): T {
   }, [value, delay])
 
   return debouncedValue
+}
+
+export function useDebouncedCallback<T extends (...args: any[]) => any>(
+  callback: T,
+  delay: number
+) {
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null)
+
+  return (...args: Parameters<T>) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+
+    setTimeoutId(
+      setTimeout(() => {
+        callback(...args)
+      }, delay)
+    )
+  }
 }
