d5985a9344f66abf7cd47b03e554ba05f9ffaea8
chmsly <chris.c.hemsley@gmail.com>
2025-01-30 18:00:37 -0700

    update app/search/page.tsx to match the repository

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,12 +1,19 @@
+"use server"
+
 import { Suspense } from "react"
 import { auth } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
+import { getProfileByUserId } from "@/db/queries/profiles-queries"
 import ChatArea from "./_components/chat-area"
 import ChatAreaSkeleton from "./_components/chat-area-skeleton"
 
 export default async function SearchPage() {
   const { userId } = auth()
-  if (!userId) redirect("/login")
+  if (!userId) return redirect("/login")
+
+  const profile = await getProfileByUserId(userId)
+  if (!profile) return redirect("/signup")
+  if (profile.membership === "free") return redirect("/pricing")
 
   return (
     <Suspense fallback={<ChatAreaSkeleton />}>
