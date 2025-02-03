5d238cd626c3adffaf41f01d2c7bf2f31824875b
chmsly <chris.c.hemsley@gmail.com>
2025-02-03 12:44:14 -0700

    update app/search/_components/chat-area-skeleton.tsx

--- app/search/_components/chat-area-skeleton.tsx
+++ app/search/_components/chat-area-skeleton.tsx
@@ -5,12 +5,26 @@ import { SearchInput } from "./search-input"
 
 export default function ChatAreaSkeleton() {
   return (
-    <div className="flex h-full flex-col gap-4 p-4">
-      <SearchInput className="mx-auto w-full max-w-2xl" onSearch={() => {}} />
-      <div className="grow space-y-4">
-        {[...Array(5)].map((_, i) => (
-          <Skeleton key={i} className="h-20 w-full" />
+    <div className="flex h-full flex-col">
+      <div className="flex-1 space-y-4 p-4">
+        {[...Array(3)].map((_, i) => (
+          <div key={i} className="flex items-start gap-4">
+            <Skeleton className="size-6 rounded" />
+            <div className="flex-1 space-y-2">
+              <Skeleton className="h-4 w-3/4" />
+              <Skeleton className="h-4 w-1/2" />
+            </div>
+          </div>
         ))}
+      </div>
+
+      <div className="p-4">
+        <SearchInput
+          className="mx-auto w-full"
+          onSearch={() => {}}
+          disabled
+          value=""
+        />
       </div>
     </div>
   )
