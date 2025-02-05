6197ad2ee92198031bccb940a36f627d19ce1b28
chmsly <chris.c.hemsley@gmail.com>
2025-02-05 16:58:51 -0700

    update app/search/_components/sidebar-container.tsx

--- app/search/_components/sidebar-container.tsx
+++ app/search/_components/sidebar-container.tsx
@@ -2,7 +2,7 @@
 
 import { auth } from "@clerk/nextjs/server"
 import { getChatsByUserId } from "@/db/queries/chats-queries"
-import Sidebar from "./sidebar"
+import SidebarClient from "./sidebar-client"
 
 export default async function SidebarContainer() {
   const { userId } = auth()
@@ -10,5 +10,5 @@ export default async function SidebarContainer() {
 
   const chats = await getChatsByUserId(userId)
 
-  return <Sidebar className="w-64 border-r" initialChats={chats} />
+  return <SidebarClient className="w-64 border-r" chats={chats} />
 }
