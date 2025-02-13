172c7abb8e2360811009bf793c415df6bcef7f77
chmsly <chris.c.hemsley@gmail.com>
2025-02-13 16:00:38 -0700

    create app/api/webhooks/clerk/route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,92 +1,67 @@
-import {
-  manageSubscriptionStatusChange,
-  updateStripeCustomer
-} from "@/actions/stripe-actions"
-import { stripe } from "@/lib/stripe"
+import { createProfile } from "@/db/queries/profiles-queries"
+import { WebhookEvent } from "@clerk/nextjs/server"
 import { headers } from "next/headers"
-import Stripe from "stripe"
-
-const relevantEvents = new Set([
-  "checkout.session.completed",
-  "customer.subscription.updated",
-  "customer.subscription.deleted"
-])
+import { NextResponse } from "next/server"
+import { Webhook } from "svix"
 
 export async function POST(req: Request) {
-  const body = await req.text()
-  const sig = headers().get("Stripe-Signature") as string
-  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET
-  let event: Stripe.Event
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET
 
-  try {
-    if (!sig || !webhookSecret) {
-      throw new Error("Webhook secret or signature missing")
-    }
-
-    event = stripe.webhooks.constructEvent(body, sig, webhookSecret)
-  } catch (err: any) {
-    console.error(`Webhook Error: ${err.message}`)
-    return new Response(`Webhook Error: ${err.message}`, { status: 400 })
-  }
-
-  if (relevantEvents.has(event.type)) {
-    try {
-      switch (event.type) {
-        case "customer.subscription.updated":
-        case "customer.subscription.deleted":
-          await handleSubscriptionChange(event)
-          break
-
-        case "checkout.session.completed":
-          await handleCheckoutSession(event)
-          break
-
-        default:
-          throw new Error("Unhandled relevant event!")
-      }
-    } catch (error) {
-      console.error("Webhook handler failed:", error)
-      return new Response(
-        "Webhook handler failed. View your nextjs function logs.",
-        {
-          status: 400
-        }
-      )
-    }
-  }
-
-  return new Response(JSON.stringify({ received: true }))
-}
-
-async function handleSubscriptionChange(event: Stripe.Event) {
-  const subscription = event.data.object as Stripe.Subscription
-  const productId = subscription.items.data[0].price.product as string
-  await manageSubscriptionStatusChange(
-    subscription.id,
-    subscription.customer as string,
-    productId
-  )
-}
-
-async function handleCheckoutSession(event: Stripe.Event) {
-  const checkoutSession = event.data.object as Stripe.Checkout.Session
-  if (checkoutSession.mode === "subscription") {
-    const subscriptionId = checkoutSession.subscription as string
-    await updateStripeCustomer(
-      checkoutSession.client_reference_id as string,
-      subscriptionId,
-      checkoutSession.customer as string
+  if (!WEBHOOK_SECRET) {
+    throw new Error(
+      "Please add CLERK_WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
     )
+  }
 
-    const subscription = await stripe.subscriptions.retrieve(subscriptionId, {
-      expand: ["default_payment_method"]
+  // Get the headers
+  const headerPayload = headers()
+  const svix_id = headerPayload.get("svix-id")
+  const svix_timestamp = headerPayload.get("svix-timestamp")
+  const svix_signature = headerPayload.get("svix-signature")
+
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new NextResponse("Error occured -- no svix headers", {
+      status: 400
+    })
+  }
+
+  // Get the body
+  const payload = await req.json()
+  const body = JSON.stringify(payload)
+
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET)
+
+  let evt: WebhookEvent
+
+  // Verify the payload with the headers
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature
+    }) as WebhookEvent
+  } catch (err) {
+    console.error("Error verifying webhook:", err)
+    return new NextResponse("Error occured", {
+      status: 400
+    })
+  }
+
+  // Get the ID and type
+  const { id } = evt.data
+  const eventType = evt.type
+
+  if (eventType === "user.created") {
+    const profile = await createProfile({
+      id,
+      userId: id,
+      membership: "free"
     })
 
-    const productId = subscription.items.data[0].price.product as string
-    await manageSubscriptionStatusChange(
-      subscription.id,
-      subscription.customer as string,
-      productId
-    )
+    return NextResponse.json({ profile })
   }
+
+  return new NextResponse("", { status: 201 })
 }
