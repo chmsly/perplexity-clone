cf374e00cfb0ffa3da4aa433f502680228766ecc
chmsly <chris.c.hemsley@gmail.com>
2025-02-13 15:58:46 -0700

    create app/search/page.tsx

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,7 +3,6 @@
 import { Suspense } from "react"
 import { auth } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
-import { getProfileByUserId } from "@/db/queries/profiles-queries"
 import ChatArea from "./_components/chat-area"
 import ChatAreaSkeleton from "./_components/chat-area-skeleton"
 
@@ -11,14 +10,6 @@ export default async function SearchPage() {
   const { userId } = auth()
   if (!userId) {
     redirect("/login")
-  }
-
-  const profile = await getProfileByUserId(userId)
-  if (!profile) {
-    redirect("/signup")
-  }
-  if (profile.membership === "free") {
-    redirect("/pricing")
   }
 
   return (
